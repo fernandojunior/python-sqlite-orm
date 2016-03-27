be25c24aec15b2e8641dd26418695881a43b5bb7
Fernando Felix <fernandojr.ifcg@live.com>
2016-03-27 05:55:03 -0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,49 +1,44 @@
 import os
 from orm import Database
 
+db = Database('db.sqlite.test')
 
-def main_test():
 
-    db = Database('db.sqlite.test')
+class Post(db.Model):
 
-    class Post(db.Model):
+    def __init__(self, title, text, id=None):
+        self.id = id
+        self.title = title
+        self.text = text
 
-        def __init__(self, title, text, id=None):
-            self.id = id
-            self.title = title
-            self.text = text
+    def show(self):
+        return '%s %s' % (self.title, self.text)
 
-        def show(self):
-            return '%s %s' % (self.title, self.text)
+    @classmethod
+    def schema(cls):
+        return '''
+        drop table if exists post;
+        create table post (
+            id integer primary key autoincrement,
+            title text not null,
+            text text not null
+        );
+        '''
 
-        @classmethod
-        def schema(cls):
-            return '''
-            drop table if exists post;
-            create table post (
-                id integer primary key autoincrement,
-                title text not null,
-                text text not null
-            );
-            '''
-
-    try:
-        hello_world = Post('Hello', 'World').save()
-        assert(hello_world.id == 1)
-        hello_world.text = 'Mundo'
-        hello_world.update()
-        db.commit()
-        assert(hello_world.show() == 'Hello Mundo')
-        hello_world.delete()
-        db.commit()
-        objects = Post.manager()
-        objects.save(Post('Hello', 'World'))
-        assert(objects.get(2).public == {
-            'text': 'World', 'id': 2, 'title': 'Hello'})
-        db.close()
-        assert(objects.all() == [])
-    finally:
-        os.remove('db.sqlite.test')
-
-if __name__ == '__main__':
-    main_test()
+try:
+    hello_world = Post('Hello', 'World').save()
+    assert(hello_world.id == 1)
+    hello_world.text = 'Mundo'
+    hello_world.update()
+    db.commit()
+    assert(hello_world.show() == 'Hello Mundo')
+    hello_world.delete()
+    db.commit()
+    objects = Post.manager()
+    objects.save(Post('Hello', 'World'))
+    assert(objects.get(2).public == {
+        'text': 'World', 'id': 2, 'title': 'Hello'})
+    db.close()
+    assert(objects.all() == [])
+finally:
+    os.remove('db.sqlite.test')
