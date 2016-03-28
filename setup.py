91dd163d5fc3aa094f430ac4be693c861c44bca7
Fernando Felix <fernandojr.ifcg@live.com>
2016-03-28 06:03:56 -0300

    Add dist files.

---
+++ setup.py
@@ -0,0 +1,23 @@
+'''
+Contains informations necessaries to build, release and install a distribution.
+'''
+from setuptools import setup
+
+setup(
+    name='sqlite-orm',
+    version='0.0.1-beta',
+    author='Fernando Felix do Nascimento Junior',
+    url='https://github.com/fernandojunior/python-sqlite-orm',
+    license='MIT License',
+    description='A Python object relational mapper for SQLite.',
+    py_modules=['orm'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 3',
+    ],  # see more at https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    zip_safe=False
+)
