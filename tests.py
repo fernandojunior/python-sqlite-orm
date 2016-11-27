e675ef81e2a71d8a69c7bc0af679bf48437a1652
Fernando Felix <fernandojr.ifcg@live.com>
2016-11-27 07:46:16 -0300

    More readable code

--- tests.py
+++ tests.py
@@ -19,23 +19,16 @@ try:
     assert(post.id == 1)
     post.text = 'Hello Mundo'
     post.update()
-    try:
-        post.random = None
-        post.update(type_check=True)
-    except TypeError:
-        pass
-    else:
-        raise Exception("Failed to check type!")
     db.commit()
     assert(post.text == 'Hello Mundo')
     post.delete()
     db.commit()
     try:
-        invalidPost = Post(None).save(type_check=True)
-    except TypeError:
-        pass
+        invalid_post = Post(None).save(type_check=False)
+    except TypeError as e:
+        assert(False)
     else:
-        raise Exception("Failed to check type!")
+        assert(True)
     objects = Post.manager()
     objects.save(Post('Hello World'))
     assert(set(objects.get(2).public.keys()) == set(['id', 'text', 'random']))
